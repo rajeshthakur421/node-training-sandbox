08707dbed26f4f04dfa96bc0cb41f4ca7049fd81
rajeshthakur421 <35397258+rajeshthakur421@users.noreply.github.com>
2022-10-22 11:30:38 +0530

    Update mysql.db.connection.js

--- db/mysql.db.connection.js
+++ db/mysql.db.connection.js
@@ -1,19 +1,18 @@
-const mysql = require("mysql");
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: "localhost",
-
-  user: "u190533072_springbootapi",
-  password: "@Gk8xry4PsWyaF@",
-  database: "u190533072_springbootapit",
-  port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodetraining",
 });
-
+  
 connection.connect((err) => {
-  // connection test
-  //   if(err){console.log(err.message)}else{console.log("connected with studDB")}
-
-  err ? console.log(err.message) : console.log("connected with nodetraining");
+    // connection test
+    //   if(err){console.log(err.message)}else{console.log("connected with studDB")}
+  
+    err ? console.log(err.message) : console.log("connected with nodetraining");
 });
-
+  
 module.exports = connection;
+  
